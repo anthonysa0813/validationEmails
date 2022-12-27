d7a4a7d443eee6e3c4e01a2d3a7f3a064f523583
anthony <anthonysa0813@gmail.com>
2022-12-27 17:45:01 -0500

    cambio de url al de vercel

--- utils/apisFunctions/fetchFunctions.ts
+++ utils/apisFunctions/fetchFunctions.ts
@@ -1,13 +1,16 @@
 import { SaveEmailInterface, ObjectEmail } from "../../intefaces/index";
 
 export const sendEmailArr = async (data: any) => {
-  const response = await fetch(`http://localhost:3000/api/correos`, {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify(data),
-  });
+  const response = await fetch(
+    `https://validation-emails.vercel.app/api/correos`,
+    {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    }
+  );
   const dataResponse = await response.json();
   return dataResponse;
 };
